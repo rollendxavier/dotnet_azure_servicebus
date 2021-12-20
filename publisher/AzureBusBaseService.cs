db504f100b3c4754699df2d9ae77570ed6294b8e
rollendxavier <rollendxavier@gmail.com>
2021-12-21 07:42:25 +0800

    Initial Commit

---
+++ publisher/AzureBusBaseService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace azure_service_bus
+{
+    public class AzureBusBaseService
+    {
+        // connection string to Service Bus namespace
+        static string connectionString = @"keep it secret if it got ur SAS key";
+
+        // name of your Service Bus queue
+        static string queueName = "sbqueue";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient client;
+
+        // the sender used to publish messages to the queue
+        static ServiceBusSender sender;
+
+        // number of messages to be sent to the queue
+        private const int numOfMessages = 3;
+
+        public async Task SendMessageAsync()
+        {
+            client = new ServiceBusClient(connectionString);
+            sender = client.CreateSender(queueName);
+
+            // create a batch 
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using azure_service_bus;
+using System;
+using System.Threading.Tasks;
+
+namespace servicebus.app
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting Service");
+
+            try
+            {
+                var bus = new AzureBusBaseService();
+
+                await bus.SendMessageAsync();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
